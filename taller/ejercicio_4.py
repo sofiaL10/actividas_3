395bfca52f2f9fab69df5491626e83711bafa7ac
juan <juanfgomez6@gmail.com>
2023-08-08 13:34:26 -0500

    initial commit. ejercicio_4.py

---
+++ taller/ejercicio_4.py
@@ -0,0 +1,12 @@
+class Rectangulo:
+    def __init__(self, esquina1: float, esquina2: float):
+        self.esquina1 = esquina1
+        self.esquina2 = esquina2
+
+    def perimetro(self):
+        perimetro_final = perimetro*4
+
+if __name__ == "__main__":
+    p: Rectangulo = Rectangulo(5, 5)
+
+
