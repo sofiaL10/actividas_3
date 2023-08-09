88ecd79f2ef569fb632152325c460dba0b498316
SofiaL10 <loperadiezsofia@gmail.com>
2023-08-08 20:32:14 -0500

    add ejercicio 2 y 3

--- taller/ejercicio_2y3.py
+++ taller/ejercicio_2y3.py
@@ -1,4 +1,4 @@
-import random
+import math
 
 
 class Punto:
@@ -6,7 +6,32 @@ class Punto:
         self.x = x
         self.y = y
 
-    def mover(self):
+    def mostrar(self):
+        print(f"Coordenadas del punto: ({self.x}, {self.y})")
+
+    def mover(self, nuevo_x, nuevo_y):
+        self.x = nuevo_x
+        self.y = nuevo_y
+
+    def calcular_distancia(self, otro_punto):
+        distancia = math.sqrt((self.x - otro_punto.x) ** 2 + (self.y - otro_punto.y) ** 2)
+        return distancia
+
+
+if __name__ == "__main__":
+   punto1 = Punto(1, 2)
+   punto2 = Punto(4, 6)
+
+punto1.mostrar()
+punto2.mostrar()
+
+
+punto1.mover(3, 5)
+punto1.mostrar()
+
+
+distancia = punto1.calcular_distancia(punto2)
+print("Distancia entre los puntos:", distancia)
 
 
 
