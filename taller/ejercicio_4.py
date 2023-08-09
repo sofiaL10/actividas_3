a596289e6188da1988d9211032c33ea7e49786d0
SofiaL10 <loperadiezsofia@gmail.com>
2023-08-08 20:32:45 -0500

    add ejercicio 4

--- taller/ejercicio_4.py
+++ taller/ejercicio_4.py
@@ -1,12 +1,46 @@
+class Punto:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
 class Rectangulo:
-    def __init__(self, esquina1: float, esquina2: float):
-        self.esquina1 = esquina1
-        self.esquina2 = esquina2
+    def __init__(self, esquina_inferior, esquina_superior):
+        self.esquina_inferior = esquina_inferior
+        self.esquina_superior = esquina_superior
 
     def perimetro(self):
-        perimetro_final = perimetro*4
+        base = abs(self.esquina_superior.x - self.esquina_inferior.x)
+        altura = abs(self.esquina_superior.y - self.esquina_inferior.y)
+        perimetro = 2 * (base + altura)
+        return perimetro
+
+
+    def area(self):
+        base = abs(self.esquina_superior.x - self.esquina_inferior.x)
+        altura = abs(self.esquina_superior.y - self.esquina_inferior.y)
+        area = base * altura
+        return area
+
+
+    def es_cuadrado(self):
+        base = abs(self.esquina_superior.x - self.esquina_inferior.x)
+        altura = abs(self.esquina_superior.y - self.esquina_inferior.y)
+        return base == altura
 
 if __name__ == "__main__":
-    p: Rectangulo = Rectangulo(5, 5)
+    punto_inferior = (1, 2)
+    punto_superior = (4, 5)
+
+rectangulo = Rectangulo(punto_inferior, punto_superior)
+
+perimetro = rectangulo.perimetro()
+area = rectangulo.area()
+print(f"Perímetro del rectángulo: ", perimetro)
+print("Área del rectángulo: ", area)
 
 
+if rectangulo.es_cuadrado():
+    print("El rectángulo es un cuadrado.")
+else:
+    print("El rectángulo no es un cuadrado.")
